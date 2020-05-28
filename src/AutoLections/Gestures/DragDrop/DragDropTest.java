90f8af4280a2fe793e7b89d6751b33b4ce93626e
Optic969 <optic969@tut.by>
2020-05-28 18:10:50 +0300

    Added Gestures tests "Drag and Drop"

--- src/AutoLections/Gestures/DragDrop/DragDropTest.java
+++ src/AutoLections/Gestures/DragDrop/DragDropTest.java
@@ -6,12 +6,16 @@ import io.appium.java_client.android.AndroidElement;
 import InstalationTest.InstallTest;
 import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
+
+import io.appium.java_client.touch.WaitOptions;
 import org.openqa.selenium.WebElement;
 
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 
 import java.net.MalformedURLException;
 import static java.time.Duration.ofSeconds;
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 
@@ -23,8 +27,12 @@ public class DragDropTest extends InstallTest {
         driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
         driver.findElementByXPath("//android.widget.TextView[@text='Drag and Drop']").click();
         TouchAction action = new TouchAction(driver);
-        WebElement webElement = driver.findElementByXPath("//*[@content-desc='15']");
-        WebElement webElement1 = driver.findElementByXPath("//*[@content-desc='15']");
+        //WebElement webElement = driver.findElementByXPath("//*[@index='1']");
+        //WebElement webElement1 = driver.findElementByXPath("//*[@index='4']");
+        WebElement source=driver.findElementsByClassName("android.view.View").get(0);
+        WebElement destination=driver.findElementsByClassName("android.view.View").get(1);
+        action.longPress(longPressOptions().withElement(element(source))).moveTo(element(destination)).release().perform();
+        action.longPress(element(source)).moveTo(element(destination)).release().perform();//second variant
 
 
 
